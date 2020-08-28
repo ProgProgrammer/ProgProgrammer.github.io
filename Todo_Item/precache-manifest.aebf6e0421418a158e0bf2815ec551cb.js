b14450c83907da54e11518c8bf435f59768fbc0f
ProgProgrammer <progr.programmer@yandex.ru>
2020-08-28 22:00:31 +0400

    Todo-item: added files.

---
+++ Todo_Item/precache-manifest.aebf6e0421418a158e0bf2815ec551cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a6c25831b193e5da2254fa8d7a1cc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebfec91c47810c0ce8ba",
+    "url": "/static/css/main.e2f86863.chunk.css"
+  },
+  {
+    "revision": "0776fcf8132cb2061e84",
+    "url": "/static/js/2.213e5924.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.213e5924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebfec91c47810c0ce8ba",
+    "url": "/static/js/main.b1692c64.chunk.js"
+  },
+  {
+    "revision": "45d62cc8786fead2ed2f",
+    "url": "/static/js/runtime-main.b3475604.js"
+  }
+]);

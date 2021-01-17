eb601dccad9ebb54b73aeeabd081c331ef49d77c
ProgProgrammer <progr.programmer@yandex.ru>
2021-01-17 15:46:15 +0300

    Our_Best_Blogs_Ever: spinner bugs fixed.

--- Our_Best_Blogs_Ever/js/articles_original_code.js
+++ Our_Best_Blogs_Ever/js/articles_original_code.js
@@ -2,19 +2,57 @@
 {
     window.addEventListener('DOMContentLoaded', () =>
     {
+        const array_id = [];
         let counter = 0;
-        let request = true;
         const step = 6;
+        let block_button = false;
+        let block_error = false;
         const start_spinner = "start";
         const button = document.querySelector(".button");
         const block = document.querySelector(".blocks .blocks-row");
         const block_spinner = document.querySelector(".spinner");
 
-        const spinner = (command, error, error_articles) =>
+        const article_id = (id) =>
+        {
+            const articles = document.querySelectorAll(".block_theme_article");
+            for (let i = 0; i < articles.length; i++)
+            {
+                if (Number(articles[i].dataset.id) === id)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        const fetch_id = (id) =>
+        {
+            if (article_id(id) === false)
+            {
+                fetch('https://jsonplaceholder.typicode.com/posts/' + id)
+                    .then((response) => response.json())
+                    .then((json) => add_blocks(json, 1, id))
+                    .catch((error) => { setTimeout(fetch_id, 1000, id) } );
+            }
+        }
+
+        const check_array_id = () =>
+        {
+            for (let i = 0; i < array_id.length; i++)
+            {
+                if (array_id[i] !== i + 1)
+                {
+                    fetch_id(i + 1);
+                }
+            }
+        }
+
+        const spinner = (command, error, error_articles, number) =>
         {
             const spinner_element = document.querySelector(".loadingio-spinner-double-ring-mmxwcmu6s6");
             if (command === start_spinner)
             {
+                block_error = true;
                 block_spinner.style.display = "flex";
                 if (spinner_element === null)
                 {
@@ -41,9 +79,10 @@
             {
                 if (spinner_element !== null)
                 {
+                    check_array_id();
+                    block_error = false;
                     spinner_element.remove();
                     block_spinner.style.display = "none";
-                    request = true;
                 }
             }
         }
@@ -61,7 +100,20 @@
             return number;
         }
 
-        const add_blocks = (json, counter_articles) =>
+        const check_id = (id) =>
+        {
+            for (let a = 0; a < array_id.length; a++)
+            {
+                if (array_id[a] === id)
+                {
+                    console.log("id_error = " + id);
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        const add_blocks = (json, counter_articles, i) =>
         {
             const spinner_element = document.querySelector(".loadingio-spinner-double-ring-mmxwcmu6s6");
             if (counter_articles === step &&
@@ -69,7 +121,7 @@
             {
                 spinner("off");
             }
-            if (json.id !== undefined)
+            if (json.id !== undefined && check_id(json.id) === true)
             {
                 const string = '<article class="col-12 col-lg-6 block block_theme_article" data-id="' + json.id + '">' +
                     '<div class="block__text-block block__text-block_theme_article">' +
@@ -85,10 +137,12 @@
                     '</div>' +
                     '</article>';
                 block.insertAdjacentHTML('beforeend', string);
+                array_id[i - 1] = json.id;
             }
-            if (counter_articles === step)
+            if (counter_articles === step && json.id !== undefined)
             {
-                request = true;
+                block_button = false;
+                counter++;
             }
         }
 
@@ -96,21 +150,21 @@
         {
             let counter_articles = 0;
             let error_articles = 0;
-            request = false;
             for (let i = value; i < number; i++) {
                 fetch('https://jsonplaceholder.typicode.com/posts/' + i)
                     .then((response) => { counter_articles++; return response.json(); })
-                    .then((json) => add_blocks(json, counter_articles))
-                    .catch((error) => { error_articles++; spinner(start_spinner, error, error_articles); } );
+                    .then((json) => add_blocks(json, counter_articles, i))
+                    .catch((error) => { error_articles++; spinner(start_spinner, error, error_articles, number); } );
             } // - код для запроса новых данных
         }
 
         const start_loaded_articles = (value) =>
         {
-            if ((request === false && value !== "request_button") ||
-                (request === true && value === "request_button")) {
+            if ((value === "request" && block_error === true) ||
+                (value === "request_button" && block_button === false && block_error !== true))
+            {
                 const arr = [];
-                let articles = document.querySelectorAll(".blocks .block_theme_article");
+                const articles = document.querySelectorAll(".blocks .block_theme_article");
                 if (counter === 0)
                 {
                     loaded_articles(step + 1);
@@ -123,13 +177,13 @@
                     let id = max_number(arr) + 1;
                     loaded_articles(id + step, id);
                 }
-                counter++;
             }
         }
 
         button.addEventListener('click', () =>
         {
             start_loaded_articles("request_button");
+            block_button = true;
         });
     });
 })
